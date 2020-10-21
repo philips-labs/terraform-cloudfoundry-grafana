9d617588a482c77c10fc2168c53bf2880208d73d
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-10-21 16:04:56 +0200

    Fix typos

--- main.tf
+++ main.tf
@@ -37,14 +37,14 @@ resource "cloudfoundry_app" "grafana" {
 
 resource "cloudfoundry_service_instance" "database" {
   name         = "grafana-rds"
-  space        = data.cloudfoudry_space.space.id
+  space        = data.cloudfoundry_space.space.id
   service_plan = data.cloudfoundry_service.rds.service_plans[var.db_plan]
   json_params  = var.db_json_params
 }
 
 resource "cloudfoundry_service_key" "database_key" {
   name    = "key"
-  service = cloudfoundry_service_instance.database.id
+  service_instance = cloudfoundry_service_instance.database.id
 }
 
 resource "cloudfoundry_route" "grafana" {
