ebdae7235ca90c7c814d85b955ba0b5ac90f1d28
andriy.pentsak <andriy.pentsak@vitech.com.ua>
2021-01-04 13:06:33 +0200

    added grafana id into output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "grafana_endpoint" {
   description = "The endpoint where Grafana is reachable on"
   value       = cloudfoundry_route.grafana.endpoint
 }
+
+output "grafana_id" {
+  description = "The Grafana's id"
+  value       = cloudfoundry_app.grafana.id
+}
