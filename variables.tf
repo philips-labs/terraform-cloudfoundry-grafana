97c86b47a2f4613adcbea5ebacfe592486bd9386
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2022-11-29 13:45:41 +0100

    Bump Grafana to 9.2.6

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "grafana_image" {
   type        = string
   description = "Grafana Docker image to use"
-  default     = "grafana/grafana:9.2.0"
+  default     = "grafana/grafana:9.2.6"
 }
 variable "cf_org_name" {
   type        = string
